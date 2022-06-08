e117378e369d39d2457cbb48ddaa40e931c26104
GadisaAsfaw <gadisasfaw@gmail.com>
2022-06-08 16:51:11 -0400

    task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_m = []
+    for i in range(0, len(matrix)):
+        new_m.append([])
+        for j in range(0, len(matrix[i])):
+            new_m[i].append( matrix[i][j] ** 2)
+    return new_m
