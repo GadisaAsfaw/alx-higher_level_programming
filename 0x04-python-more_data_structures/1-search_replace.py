fc36cc7667d23624bd901b1a11bdf63aa2dd7538
GadisaAsfaw <gadisasfaw@gmail.com>
2022-06-08 17:07:47 -0400

    task-1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+def search_replace(my_list, search, replace):
+    new_l = []
+    for i in my_list:
+        if i == search:
+            new_l.append(replace)
+        else:
+            new_l.append(i)
+    return new_l
