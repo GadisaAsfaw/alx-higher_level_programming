3cc4ff128408c3ad137340e51aaa7a539776945c
GadisaAsfaw <gadisasfaw@gmail.com>
2022-06-08 14:55:51 -0400

    task 4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    arr = dir(hidden_4)
+    for i in range(0, len(arr)):
+        if arr[i][0:2] != "__":
+            print("{}".format(arr[i]))
