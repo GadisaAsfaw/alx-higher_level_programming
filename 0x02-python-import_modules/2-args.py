548e1674fc9476c46280cc434bc2b9f4aafb5d42
GadisaAsfaw <gadisasfaw@gmail.com>
2022-06-08 14:36:32 -0400

    dbg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,10 @@
 if __name__ == "__main__":
     import sys
     num = len(sys.argv)
-    if num == 2:
-        print("{} argument.".format(num - 1))
+    if num == 1:
+        print("{} arguments.".format(num - 1))
+    elif num == 2:
+        print("{} argument:".format(num - 1))
     else:
         print("{} arguments:".format(num - 1))
 
