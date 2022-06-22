a84693acc51fde3436edea8690b676387003f664
GadisaAsfaw <gadisasfaw@gmail.com>
2022-06-22 05:01:17 +0300

    task-1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class initialise its size
+"""
+
+
+class Square:
+    '''Class Square with private instance attribute size
+    '''
+    def __init__(self, size):
+        self.__size = size
+    pass
