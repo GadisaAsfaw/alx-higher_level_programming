5446e9434acba8ceca2550ad15ed5f0da44bab76
GadisaAsfaw <gadisasfaw@gmail.com>
2022-07-06 23:15:24 +0300

    pep8

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""Module 101-add_attribute.
+Checks if an attribute can be added to an object.
+"""
+
+
 def add_attribute(*args):
+    """adds a new attribute to an object if it’s possible"""
+
     if "main" in str(type(args[0])):
         setattr(args[0], args[1], args[2])
     else:
