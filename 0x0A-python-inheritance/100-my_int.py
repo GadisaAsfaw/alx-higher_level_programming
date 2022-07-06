0598ac4672f4db52ce99d9cfff39725fa1e5a1ee
GadisaAsfaw <gadisasfaw@gmail.com>
2022-07-06 23:12:02 +0300

    dbg

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""Module 100-my_int.
+Creates a class that inherits from int.
+"""
+
+
 class MyInt(int):
+    """Class inheriting from int,
+    But reverses the behavior of != and ==.
+    """
+
     def __init__(self, value):
         self.num = value
 
