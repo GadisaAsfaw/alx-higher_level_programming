c8eefd6c03a87ae210f437397568e21d22506d53
GadisaAsfaw <gadisasfaw@gmail.com>
2022-07-11 23:40:23 +0300

    pep8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -159,6 +159,7 @@ class Rectangle(Base):
                     self.x = value
                 if key == "y":
                     self.y = value
+
     def to_dictionary(self):
         """Returns the dictionary representation of a Rectangle."""
 
